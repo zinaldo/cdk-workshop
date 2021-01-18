08169c93246d675009ff5c58e95541c004700094
Zinaldo Barros <zinaldo@gmail.com>
2021-01-17 23:46:43 -0300

    halfway through the workshop

---
+++ lib/cdk-workshop-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import { HitCounter } from './hitcounter';
+
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    })
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    })
+  }
+}
---
+++ lib/hitcounter.ts
@@ -0,0 +1,37 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamo from '@aws-cdk/aws-dynamodb';
+
+export interface HitCounterProps {
+    downstream: lambda.IFunction;
+}
+
+export class HitCounter extends cdk.Construct {
+
+    public readonly handler: lambda.IFunction;
+
+    constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+        super(scope, id);
+
+        const table = new dynamo.Table(this, 'Hits', {
+            partitionKey: {
+                name: 'path',
+                type: dynamo.AttributeType.STRING
+            }
+        });
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_12_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName
+            }
+        });
+
+        table.grantReadWriteData(this.handler);
+
+        props.downstream.grantInvoke(this.handler);
+    }
+}
